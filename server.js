f8d673d190f3258cd4b23e3c985a97abae5a3db2
stefano1977 <32017555+stefano1977@users.noreply.github.com>
2017-09-17 09:58:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-comst port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
